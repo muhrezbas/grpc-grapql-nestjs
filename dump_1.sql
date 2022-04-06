7beac5d429f27fcec8fe2c5a17533bcb904d6a2a
Reza Basuki <rezabasuki@Rezas-MacBook-Pro.local>
2022-04-06 11:41:45 +0700

    dump

--- dump_1.sql
+++ dump_1.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 5.7.37, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.28, for macos12.2 (x86_64)
 --
--- Host: localhost    Database: bareksa_test
+-- Host: localhost    Database: news
 -- ------------------------------------------------------
--- Server version	8.0.22
+-- Server version	8.0.28
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,7 +21,7 @@
 
 DROP TABLE IF EXISTS `news`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `news` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `topic_id` int unsigned NOT NULL,
@@ -47,12 +47,64 @@ INSERT INTO `news` VALUES (1,1,'how to start investment','publish','2022-03-13 2
 UNLOCK TABLES;
 
 --
+-- Table structure for table `reporter`
+--
+
+DROP TABLE IF EXISTS `reporter`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reporter` (
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `id` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `topic_id` int NOT NULL,
+  `asal` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reporter`
+--
+
+LOCK TABLES `reporter` WRITE;
+/*!40000 ALTER TABLE `reporter` DISABLE KEYS */;
+INSERT INTO `reporter` VALUES ('Tono','2acd9c62-de71-4e98-be14-af1dec86fffa',1,'Jakarta'),('Sigit','381ccc22-7329-4f34-ac7b-31eef7abbe04',2,'Jakarta'),('Dick Grayson','41599b6b-939c-4ff6-be46-b270a433fe68',1,'WNA'),('Eca','4ce373e1-d0cc-443d-9738-b4206c3e113d',2,'Jakarta'),('Tia','6294cbf3-4a90-425c-9d11-cbc40e446b20',1,'Medan'),('Todd','741ed427-5bd2-4f6e-9e8c-e5a16d31294c',1,'WNA'),('Sigit','b0f8baa4-76e5-40b1-a80d-4bb19a3cded8',2,'Jakarta'),('Barbara','e58bc193-90de-4b08-8366-cfff3ee8f66a',1,'WNA');
+/*!40000 ALTER TABLE `reporter` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reporter_old`
+--
+
+DROP TABLE IF EXISTS `reporter_old`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reporter_old` (
+  `name` varchar(100) DEFAULT NULL,
+  `id` varchar(10) NOT NULL,
+  `topic_id` int NOT NULL,
+  `asal` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reporter_old`
+--
+
+LOCK TABLES `reporter_old` WRITE;
+/*!40000 ALTER TABLE `reporter_old` DISABLE KEYS */;
+INSERT INTO `reporter_old` VALUES ('Tono','1',1,'Jakarta'),('Sigit','2',2,'Bandung'),('reza','3',1,'jakarta');
+/*!40000 ALTER TABLE `reporter_old` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tags`
 --
 
 DROP TABLE IF EXISTS `tags`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tags` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(30) NOT NULL,
@@ -79,7 +131,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `tags_news`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tags_news` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `news_id` int unsigned NOT NULL,
@@ -110,7 +162,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `topic`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `topic` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(30) NOT NULL,
@@ -140,4 +192,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-03-28 15:04:41
+-- Dump completed on 2022-04-06 11:40:50
